98eb1b51881e828a14816d5d42d1de8fe1747ab4
Ahmet Salih Balandi <salihbalandi@gmail.com>
2026-03-28 12:59:09 +0000

    Autonomous Update: ders3_17.m (Part 1)

---
+++ ders3_17.m
@@ -0,0 +1,14 @@
+% Example 1.7
+% Matlab code
+
+D=imread('onion.png');
+
+Dred=D(:,:,1);
+Dgreen=D(:,:,2);
+Dblue=D(:,:,3);
+
+subplot(2,2,1); imshow(D); axis image;
+
+subplot(2,2,2); imshow(Dred); title('red');
+subplot(2,2,3); imshow(Dgreen); title('green');
+subplot(2,2,4); imshow(Dblue); title('blue');
