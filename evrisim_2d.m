b06cfb08b1c460d08886a26a11718f440f71e3de
Ahmet Salih Balandi <salihbalandi@gmail.com>
2026-04-30 14:00:05 +0000

    Autonomous Update: evrisim_2d.m (Part 1)

---
+++ evrisim_2d.m
@@ -0,0 +1,13 @@
+clc;clear all;
+
+A = imread('cameraman.jpg');           
+PSF = fspecial('gaussian', [5 5], 2);   
+h = fspecial('motion', 10, 45);         
+B = conv2(PSF, A);                      
+C = imfilter(A, h, 'replicate');        
+D = conv2(A, A);                        
+
+subplot(2,2,1), imshow(A);              
+subplot(2,2,2), imshow(B, []);          
+subplot(2,2,3), imshow(C, []);          
+subplot(2,2,4), imshow(D, []);          
